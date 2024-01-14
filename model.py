39f11a8ff9c14e272e4cebd127b9f2040a0005ea
Onyiriuba Leonard <leonardleo110011@gmail.com>
2024-01-14 11:16:06 +0000

    Updates

--- model.py
+++ model.py
@@ -9,9 +9,14 @@ Created on Mon Jan  8 22:05:30 2024
 import pandas as pd
 import numpy as np
 import joblib
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
 from buildml.automate import SupervisedLearning
 
-# Get Dataset
+# G E T   D A T A S E T 
 training_data = pd.read_csv("train.csv")
 test_data = pd.read_csv("test.csv")
 
@@ -23,18 +28,43 @@ automate_test = SupervisedLearning(test_data)
 
 automate = [automate_training, automate_test]
 
-# Exploratory Data Analysis
+# E X P L O R A T O R Y   D A T A   A N A L Y S I S 
 training_eda = automate_training.eda()
 test_eda = automate_test.eda()
 
-# Data Cleaning and Transformation
-training_eda_visual = automate_training.eda_visual(y = "Loan Status", figsize_barchart = (55, 10), figsize_heatmap = (15, 10), figsize_histogram=(35, 20))
+# D A T A   C L E A N I N G   A N D   T R A N S F O R M A T I O N 
+# training_eda_visual = automate_training.eda_visual(y = "Loan Status", figsize_barchart = (55, 10), figsize_heatmap = (15, 10), figsize_histogram=(35, 20))
 
 for data in automate:
     data.drop_columns(["ID", "Batch Enrolled"])
     data.categorical_to_numerical()
-    data.select_dependent_and_independent(predict = "Loan Status")
-    clean = data.get_dataset()
+
+drop_column = automate_test.drop_columns("Loan Status")
+select_variables = automate_training.select_dependent_and_independent(predict = "Loan Status")
     
+# To train this model, we would use the training data as the main data for this project. It will be split into further train and test sets.
+
+# F U R T H E R   D A T A   P R E P A R A T I O N   A N D   S E G R E G A T I O N
+training_data_clean = automate_training.get_dataset()
+test_data_clean = automate_test.get_dataset()
+
+unbalanced_dataset_check = automate_training.count_column_categories(column = "Loan Status")
+split_data = automate_training.split_data()
+fix_unbalanced_data = automate_training.fix_unbalanced_dataset(sampler = "SMOTE", k_neighbors = 5, random_state = 0)
+
+check_unbalanced_data_fix = automate_training.count_column_categories(column = "Loan Status")
+
+# M O D E L   B U I L D I N G 
+classifiers = [LogisticRegression(random_state = 0),
+               SVC(),
+               RandomForestClassifier(random_state = 0),
+               DecisionTreeClassifier(random_state = 0),
+               XGBClassifier(random_state = 0)
+               ]
+# build_model = automate_training.build_multiple_classifiers(classifiers = classifiers,
+#                                                            kfold = 10,
+#                                                            cross_validation = True,
+#                                                            graph = True
+#                                                            )
     
 
